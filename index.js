14bd506ca443de2f0d58e01877fd6f02ab3444b9
hyperrex <mrwendell@gmail.com>
2018-05-20 21:55:24 -0700

    Template both review and test files

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
  * @param2: num2
  * @return: number
  **/
-function add(num1,num2){
-  return num1+num2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
 /**
@@ -16,68 +16,145 @@ function add(num1,num2){
  * @param2: num2
  * @return: number
  **/
-function subtract(num1,num2){
-  return num1-num2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-//ANNOTATE HERE
-function calculateSum(array){
+/**
+ * @name: calculateSum
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function calculateSum(array) {
   //WRITE YOUR CODE HERE
 }
 
-//don't look up the answer!
-function forEach(){
+/**
+ * @name: forEach
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function forEach() {
 
 }
 
-function filter(){
+/**
+ * @name: filter
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function filter() {
 
 }
 
-function reverseLettersInPlace(){
+/**
+ * @name: reverseLettersInPlac
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function reverseLettersInPlace() {
 
 }
 //reverseLettersInPlace("Hi justin") => "iH nitsuj"
 //reverseLettersInPlace(" A simple challenge! ") => => " A elpmis !egnellahc "
 
-function allSimpleCombinations(){
+/**
+ * @name: allSimpleCombinations
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function allSimpleCombinations() {
 
 }
 //allSimpleCombinations('cat')  => ['c','ca','cat','a','at','t']
 //allSimpleCombinations('talk') => ['t','ta','tal','talk','a','al','alk','l','lk','k'];
 
-function alphabeticalOrder(){
+/**
+ * @name: alphabeticalOrder
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function alphabeticalOrder() {
 
 }
 //allSimpleCombinations('talk') => ''
 //allSimpleCombinations('sailormoon')  => 'ailmnooors'
 
-function returnLongestWord(){
+/**
+ * @name: returnLongestWord
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function returnLongestWord() {
 
 }
 //returnLongestWord('short sentence alphabetical') => 'alphabetical'
 //returnLongestWord('cute pretty beautiful pulchritudinous') => 'pulchritudinous'
 
-function isPrime(){
+/**
+ * @name: isPrime
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function isPrime() {
 
 }
 //isPrime(7)  => true
 //isPrime(12) => false
 
-function returnCoinBreakdown(){
+/**
+ * @name: returnCoinBreakdown
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function returnCoinBreakdown() {
 
 }
 //returnCoinBreakdown(46) => {25:1,10:1,5:2,1:1}
 //returnCoinBreakdown(19) => {25:0,10:1,5:1,1:4}
 
-function countVowels(){
+/**
+ * @name: countVowels
+ * @description 
+ * @param1:
+ * @param2:
+ * @return:
+ **/
+function countVowels() {
 
 }
 //countVowels('apple')  => 2
 //countVowels('tomato') => 3
 
 module.exports = {
-  add:add,
-  subtract:subtract,
-  //add your functions here
-}
+  add: add,
+  subtract: subtract,
+  calculateSum: calculateSum,
+  forEach: forEach,
+  filter: filter,
+  reverseLettersInPlace: reverseLettersInPlace,
+  allSimpleCombinations: allSimpleCombinations,
+  alphabeticalOrder: alphabeticalOrder,
+  returnLongestWord: returnLongestWord,
+  isPrime: isPrime,
+  returnCoinBreakdown: returnCoinBreakdown,
+  countVowels: countVowels
+};
--- test/writeTestsHere.js
+++ test/writeTestsHere.js
@@ -1,41 +1,59 @@
 const assert = require('chai').assert;
 const allFunctions = require('../index');
 
-describe('all tests for your functions',()=>{
+describe('all tests for your functions', () => {
 
-  //ADD
-  describe('add tests',()=>{
+  // add
+  describe('add tests', () => {
     const addFunc = allFunctions.add;
-    it('should add positive numbers',()=>{
-      assert.equal(addFunc(1,3),4);
-      assert.equal(addFunc(8,2),10);
+    it('should add positive numbers', () => {
+      assert.equal(addFunc(1, 3), 4);
+      assert.equal(addFunc(8, 2), 10);
     });
-    it('should add negative numbers',()=>{
-      assert.equal(addFunc(-1,-3),-4);
-      assert.equal(addFunc(-10,-3),-13);
+    it('should add negative numbers', () => {
+      assert.equal(addFunc(-1, -3), -4);
+      assert.equal(addFunc(-10, -3), -13);
     });
-    it('should output number',()=>{
-      assert.isNumber(addFunc(5,5));
+    it('should output number', () => {
+      assert.isNumber(addFunc(5, 5));
     });
   })
 
-  //SUBTRACT
-  describe('subtract tests',()=>{
+  // subtract
+  describe('subtract tests', () => {
     const subFunc = allFunctions.subtract;
 
-    it('should subtract positive numbers',()=>{
-      assert.equal(subFunc(1,3),-2);
-      assert.equal(subFunc(100,87),13);
+    it('should subtract positive numbers', () => {
+      assert.equal(subFunc(1, 3), -2);
+      assert.equal(subFunc(100, 87), 13);
     })
-    it('should subtract negative numbers',()=>{
-      assert.equal(subFunc(-1,-3),2)
-      assert.equal(subFunc(-39,-11),-28)
+    it('should subtract negative numbers', () => {
+      assert.equal(subFunc(-1, -3), 2)
+      assert.equal(subFunc(-39, -11), -28)
     })
-    it('should output number',()=>{
-      assert.isNumber(subFunc(8,1));
+    it('should output number', () => {
+      assert.isNumber(subFunc(8, 1));
     });
   })
 
-  //
+  // calculateSum
+
+  // forEach
+
+  // filter
+
+  // reverseLettersInPlace
+
+  // allSimpleCombinations
+
+  // alphabeticalOrder
+
+  // returnLongestWord
+
+  // isPrime
+
+  // returnCoinBreakdown
+
+  // countVowels
 
 })
